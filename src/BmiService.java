55beb44cb37de97faaf89bafe670a7d9cfaf9d43
AntoNeo54 <7ja_reznikovih@mail.ru>
2020-11-17 23:00:21 +0700

    delete commit code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
     public float calculate(float weight, float height) {
         float Bmi = weight / (height/100 * 2);
- //       System.out.println (Bmi);
         return Bmi;
     }
 }
